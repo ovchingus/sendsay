f35c444932153c1b6e8c74f35e6a4e18a580b1c5
Vladimir Ovechkin <ovchingus@gmail.com>
2020-01-17 14:54:02 +0300

    Update Input component

--- src/components/Forms/Input/index.js
+++ src/components/Forms/Input/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 import './style.css'
 
-const Input = ({ label, placeholder, className }) => {
+const Input = ({ name, label, placeholder, className, type }) => {
   return (
     <form className={`Input ${className}`}>
       <div className='Input-label'>{label}</div>
-      <input placeholder={placeholder} className='Input-field' />
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        className='Input-field'
+      />
     </form>
   )
 }
